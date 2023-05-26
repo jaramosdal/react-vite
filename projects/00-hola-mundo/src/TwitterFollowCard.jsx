169fd03c83ca9d04106e9dda2e423fee1e7b594a
Javier Ramos Nodal <javiramos@soliss.com>
2023-05-26 13:07:43 +0200

    1:47:49

--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,8 +1,16 @@
-export function TwitterFollowCard({
-  children,
-  userName = "unknown",
-  isFollowing,
-}) {
+import { useState } from "react";
+
+export function TwitterFollowCard({ children, userName = "unknown" }) {
+  const [isFollowing, setIsFollowing] = useState(false);
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = isFollowing
+    ? "tw-followCard-button is-following"
+    : "tw-followCard-button";
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
+
   return (
     <article className="tw-followCard">
       <header className="tw-followCard-header">
@@ -18,7 +26,9 @@ export function TwitterFollowCard({
       </header>
 
       <aside>
-        <button className="tw-followCard-button">Seguir</button>
+        <button className={buttonClassName} onClick={handleClick}>
+          {text}
+        </button>
       </aside>
     </article>
   );
