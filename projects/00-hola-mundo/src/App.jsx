949a9e16d7b67163dfbe40e4869a32dffb1480fd
Javier Ramos Nodal <javiramos@soliss.com>
2023-05-26 10:38:32 +0200

    1:36:15

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -3,19 +3,16 @@ import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App() {
+  const midudev = { isFollowing: true, userName: "midudev" };
+
   return (
     <section className="App">
-      <TwitterFollowCard
-        isFollowing
-        userName="NodalJavier"
-        name="Javier Ramos Nodal"
-      />
-      <TwitterFollowCard
-        isFollowing={false}
-        userName="midudev"
-        name="Miguel Ángel Durán"
-      />
-      <TwitterFollowCard isFollowing userName="elonmusk" name="Elon Musk" />
+      <TwitterFollowCard isFollowing userName="NodalJavier">
+        Javier Ramos Nodal
+      </TwitterFollowCard>
+
+      {/* Esto es mala práctica (pasar las props así), siempre mejor separadas */}
+      <TwitterFollowCard {...midudev}>Miguel Ángel Durán</TwitterFollowCard>
     </section>
   );
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,4 +1,8 @@
-export function TwitterFollowCard({ userName, name, isFollowing }) {
+export function TwitterFollowCard({
+  children,
+  userName = "unknown",
+  isFollowing,
+}) {
   return (
     <article className="tw-followCard">
       <header className="tw-followCard-header">
@@ -8,7 +12,7 @@ export function TwitterFollowCard({ userName, name, isFollowing }) {
           src={`https://unavatar.io/${userName}`}
         />
         <div className="tw-followCard-info">
-          <strong>{name}</strong>
+          <strong>{children}</strong>
           <span className="tw-followCard-infoUserName">@{userName}</span>
         </div>
       </header>
