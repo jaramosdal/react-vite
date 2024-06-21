d469cb7760ac477daba13a077b6b950532f9dd5c
Javier Ramos Nodal <javiramos@soliss.com>
2024-06-21 09:34:53 +0200

    Curso de React desde cero: Crea un videojuego y una aplicación para aprender useState y useEffect 55:18.

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,49 +1,53 @@
 import { useState } from "react";
+import confetti from "canvas-confetti";
+import Square from "./components/Square";
+import { TURNS } from "./constants";
+import { checkWinnerFrom, checkEndGame } from "./logic/board";
+import { WinnerModal } from "./components/WinnerModal";
 import "./App.css";
-
-const TURNS = {
-  X: "x",
-  O: "o",
-};
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className = `square ${isSelected ? "is-selected" : ""}`;
-
-  const handleClick = () => {
-    updateBoard(index);
-  };
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  );
-};
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  // null es que no hay ganador, false es que hay empate
+  const [winner, setWinner] = useState(null);
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
 
   const updateBoard = (index) => {
-    if (board[index]) {
+    if (board[index] || winner) {
       return;
     }
 
     const newBoard = [...board];
     newBoard[index] = turn;
-    setBoard(newBoard);
+    setBoard(newBoard); // asíncrona
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+
+    const newWinner = checkWinnerFrom(newBoard);
+    if (newWinner) {
+      confetti();
+      setWinner(newWinner); // Si esto fuese síncrono, QUE NO LO ES,
+      console.log(winner); // Leyendo el estado... (lee el anterior, PORQUE EL ESTADO SE ACTUALIZA DE FORMA ASÍNCRONA)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
+    }
   };
 
   return (
     <main className="board">
       <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Empezar de nuevo</button>
       <section className="game">
-        {board.map((_, index) => (
+        {board.map((square, index) => (
           <Square key={index} index={index} updateBoard={updateBoard}>
-            {board[index]}
+            {square}
           </Square>
         ))}
       </section>
@@ -51,6 +55,8 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+
+      <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
   );
 }
