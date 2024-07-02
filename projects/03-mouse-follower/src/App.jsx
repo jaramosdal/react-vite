8c0ef579b2dda360dd79f1821fd831225d6e7f8d
Javier Ramos Nodal <javiramos@soliss.com>
2024-07-02 14:28:12 +0200

    project 03.

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -28,6 +28,10 @@ const FollowMouse = () => {
     };
   }, [enabled]);
 
+  // [] -> solo se ejecuta una vez cuando se monta el componente
+  // [enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
+  // undefined -> se ejecuta cada vez que se renderiza el componente
+
   // change body className
   useEffect(() => {
     document.body.classList.toggle("no-cursor", enabled);
