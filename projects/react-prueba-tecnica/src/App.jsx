40f62e555ae04349a6fb36d9b35e830e3bc43cb5
Javier Ramos Nodal <javiramos@soliss.com>
2024-07-18 07:55:53 +0200

    Loading cats.

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const CAT_ENDPOINT_RANDOM_FACT = `https://catfact.ninja/fact`;
-// xconst CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`;
+// xconst CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstThreeWords}?size=50&color=red&json=true`;
+const CAT_PREFIX_IMAGE_URL = `https://cataas.com`;
+
 const App = () => {
   const [fact, setFact] = useState("lorem ipsum cat fact whatever");
+  const [imageUrl, setImageUrl] = useState();
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
@@ -12,8 +16,18 @@ const App = () => {
         const { fact } = data;
         setFact(fact);
 
-        const firstWord = fact.split(" ")[0];
-        console.log(firstWord);
+        const firstThreeWords = fact.split(" ", 3).join(" ");
+
+        fetch(
+          `https://cataas.com/cat/says/${firstThreeWords}?size=50&color=red&json=true`
+        )
+          .then((res) => res.json())
+          .then((response) => {
+            const { _id: id } = response;
+            const url = `https://cataas.com/cat/${id}`;
+            console.log(url);
+            setImageUrl(url);
+          });
       });
   }, []);
 
@@ -21,6 +35,12 @@ const App = () => {
     <main>
       <h1>App de gatitos</h1>
       {fact && <p>{fact}</p>}
+      {imageUrl && (
+        <img
+          src={`${imageUrl}`}
+          alt={`Image extracted using the first three words for ${fact}`}
+        />
+      )}
     </main>
   );
 };
