2d3cce54a3b34681da89d0aeecca076263c9df91
Javier Ramos Nodal <javiramosnodal@gmail.com>
2024-07-19 18:09:57 +0200

    Separo los dos useEfecto.

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -2,34 +2,44 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const CAT_ENDPOINT_RANDOM_FACT = `https://catfact.ninja/fact`;
-// xconst CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstThreeWords}?size=50&color=red&json=true`;
-const CAT_PREFIX_IMAGE_URL = `https://cataas.com`;
 
 const App = () => {
-  const [fact, setFact] = useState("lorem ipsum cat fact whatever");
+  const [fact, setFact] = useState();
   const [imageUrl, setImageUrl] = useState();
 
+  // Para recuperar la cita al cargar la página
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) throw new Error("Error fetching fact.");
+        return res.json();
+      })
       .then((data) => {
         const { fact } = data;
         setFact(fact);
-
-        const firstThreeWords = fact.split(" ", 3).join(" ");
-
-        fetch(
-          `https://cataas.com/cat/says/${firstThreeWords}?size=50&color=red&json=true`
-        )
-          .then((res) => res.json())
-          .then((response) => {
-            const { _id: id } = response;
-            const url = `https://cataas.com/cat/${id}`;
-            console.log(url);
-            setImageUrl(url);
-          });
+      })
+      .catch((err) => {
+        // tanto si hay un error con la respuesta, como si lo hay con la petición
+        // si no hago el throw new Error arriba, sólo entraría aquí si el error está en la petición
       });
   }, []);
+
+  // Para recuperar la imagen cada vez que tenemos una cita nueva
+  useEffect(() => {
+    if (!fact) return;
+
+    const threeFirstWords = fact.split(" ", 3).join(" ");
+
+    fetch(
+      `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
+    )
+      .then((res) => res.json())
+      .then((response) => {
+        const { _id: id } = response;
+        const url = `https://cataas.com/cat/${id}`;
+        setImageUrl(url);
+      });
+  }, [fact]);
 
   return (
     <main>
